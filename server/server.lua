b103b9ff2dbaf39eb0bcf184aee1f442015761de
polisek <107623238+polisek@users.noreply.github.com>
2022-08-02 00:23:16 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -6,19 +6,19 @@ AddEventHandler('pls_vykup:server:sellitem', function(amount,data)
 	local src = source
     local Player = exports['qbr-core']:GetPlayer(src)
 	local amount = tonumber(amount)
-	local checkbars = Player.Functions.GetItemByName(data.item)
-	if checkbars ~= nil then
-		local amountbars = Player.Functions.GetItemByName(data.item).amount
-		if amountbars >= amount then
+	local checkitem = Player.Functions.GetItemByName(data.item)
+	if checkitem ~= nil then
+		local amountitem = Player.Functions.GetItemByName(data.item).amount
+		if amountitem >= amount then
 			totalcash = (amount * data.price) 
 			Player.Functions.RemoveItem(data.item, amount)
 			TriggerClientEvent('inventory:client:ItemBox', src, sharedItems[data.item], "remove")
 			Player.Functions.AddMoney('cash', totalcash)
-			TriggerClientEvent('QBCore:Notify', src, 9, 'Prodal si ' ..amount.. ' kusů za  $'..totalcash, 5000, 0, 'hud_textures', 'check', 'COLOR_WHITE')
+			TriggerClientEvent('QBCore:Notify', src, 9, 'You sold ' ..amount.. ' for  $'..totalcash, 5000, 0, 'hud_textures', 'check', 'COLOR_WHITE')
 		else
-			TriggerClientEvent('QBCore:Notify', src, 9, 'Nemas takovy pocet', 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
+			TriggerClientEvent('QBCore:Notify', src, 9, 'You dont have that much on you.', 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
 		end
 	else
-		TriggerClientEvent('QBCore:Notify', src, 9, 'Nemas zadne predmety', 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
+		TriggerClientEvent('QBCore:Notify', src, 9, 'You don t have an item on you', 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
 	end
 end)
