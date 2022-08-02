e7504156bf1b2052926042ee1b45b7b384bd813f
polisek <107623238+polisek@users.noreply.github.com>
2022-08-02 08:53:39 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -7,18 +7,22 @@ AddEventHandler('pls_vykup:server:sellitem', function(amount,data)
     local Player = exports['qbr-core']:GetPlayer(src)
 	local amount = tonumber(amount)
 	local checkitem = Player.Functions.GetItemByName(data.item)
-	if checkitem ~= nil then
-		local amountitem = Player.Functions.GetItemByName(data.item).amount
-		if amountitem >= amount then
-			totalcash = (amount * data.price) 
-			Player.Functions.RemoveItem(data.item, amount)
-			TriggerClientEvent('inventory:client:ItemBox', src, sharedItems[data.item], "remove")
-			Player.Functions.AddMoney('cash', totalcash)
-			TriggerClientEvent('QBCore:Notify', src, 9, 'You sold ' ..amount.. ' for  $'..totalcash, 5000, 0, 'hud_textures', 'check', 'COLOR_WHITE')
+	if amount >= 0 then
+		if checkitem ~= nil then
+			local amountitem = Player.Functions.GetItemByName(data.item).amount
+			if amountitem >= amount then
+				totalcash = (amount * data.price) 
+				Player.Functions.RemoveItem(data.item, amount)
+				TriggerClientEvent('inventory:client:ItemBox', src, sharedItems[data.item], "remove")
+				Player.Functions.AddMoney('cash', totalcash)
+				TriggerClientEvent('QBCore:Notify', src, 9, 'You sold ' ..amount.. ' for  $'..totalcash, 5000, 0, 'hud_textures', 'check', 'COLOR_WHITE')
+			else
+				TriggerClientEvent('QBCore:Notify', src, 9, 'You dont have that much on you.', 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
+			end
 		else
-			TriggerClientEvent('QBCore:Notify', src, 9, 'You dont have that much on you.', 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
+			TriggerClientEvent('QBCore:Notify', src, 9, 'You don t have an item on you', 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
 		end
 	else
-		TriggerClientEvent('QBCore:Notify', src, 9, 'You don t have an item on you', 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
-	end
+        	TriggerClientEvent('QBCore:Notify', src, 9, 'Must not be a negative value.', 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
+    	end
 end)
