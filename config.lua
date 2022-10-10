587cd0526fd5607d30cfadb728ed219e61adb8d4
polisek <107623238+polisek@users.noreply.github.com>
2022-10-10 13:53:47 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,56 +2,29 @@ Config = {}
 
 Config.VykupShops = {
     {
-        uid="vykupdreva1",  --  Unique name - Must not be the same as others
-        
-        header="Selling wood BlackWater", -- Menu title
-        pos=vector3(-872.08, -1311.51, 43.01), --Position for sell
+        uid="vykupdreva1",  -- Unique name - Must not be the same as others
+        header="Wood seller", -- Heading of menu
+        pos=vector3(-872.08, -1311.51, 43.01), -- Position
         ped={
-            enable=true, 
-            model="A_M_M_BiVWorker_01",
-            pos=vector3(-869.89, -1313.71, 43.01), 
-            head=33.03,
+            enable=true, -- PED true / false
+            model="A_M_M_BiVWorker_01", -- Model 
+            pos=vector3(-869.89, -1313.71, 43.01),  -- NPC position
+            head=33.03, -- NPC Heading
         },
         blip={
-            enable=true, 
-            bliphash = 	688589278,
-            bliptext="Wood sell",
+            enable=true,  -- Blip true / false
+            bliphash = 	688589278, -- Blip hash 
+            bliptext="Wood seller", -- Blip text
 
         },
         shopdata={
             {
-                title="Sell wood",
-                description="Selling wood",
-                price=0.2,
-                item="wood",
+                title="Wood seller", -- Heading in menu
+                description="Wood selling", -- Popis v menu
+                price=0.03, -- Price 
+                item="wood", -- Item
             },
         },
     },
 
-    {
-        uid="vykupuhlisd", ---  Unique name - Must not be the same as others
-        header="Selling coal Saint Denis",
-        pos=vector3(2578.48, -1413.23, 46.31),
-        ped={
-            enable=true, 
-            model="A_M_M_BlWLaborer_02",
-            pos=vector3(2577.73, -1413.3, 46.31), 
-            head=284.1,
-        },
-        blip={
-            enable=true, 
-            bliphash = 	688589278,
-            bliptext="Coal sell",
-
-        },
-        shopdata={
-            {
-                title="Sell coal",
-                description="Selling coal",
-                price=0.9,
-                item="coal",
-            },
-        },
-    },
 }
-
